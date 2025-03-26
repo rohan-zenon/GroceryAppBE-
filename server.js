7c706b51b07b88de9513687963b386351e3ccd96
AnupDudhe <152959908+AnupDudhe@users.noreply.github.com>
2025-03-26 18:57:31 +0530

    asd

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect("mongodb://13.126.27.181:27017/grocerydb", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
